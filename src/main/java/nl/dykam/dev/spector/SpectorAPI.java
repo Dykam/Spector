a26c4b4065c858ee33ba42f23922680312a81d96
Dykam <kramieb@gmail.com>
2014-01-21 01:38:28 +0100

    Ensure proper initialization

--- src/main/java/nl/dykam/dev/spector/SpectorAPI.java
+++ src/main/java/nl/dykam/dev/spector/SpectorAPI.java
@@ -5,7 +5,7 @@ import org.bukkit.plugin.Plugin;
 public class SpectorAPI {
     static SpectorManager manager;
     public static Spector create(Plugin creator, String name) {
-        return manager.create(creator, name);
+        return getManager().create(creator, name);
     }
 
     static void initialize() {
@@ -13,6 +13,8 @@ public class SpectorAPI {
     }
 
     public static SpectorManager getManager() {
+        if(manager == null)
+            throw new IllegalStateException("SpectorPlugin has not been initialized yet. Make sure Spector is loaded before any dependency.");
         return manager;
     }
 }
